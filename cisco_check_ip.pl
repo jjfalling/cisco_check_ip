d1ebd47fd35a5bafd5792688593c98eaca5d66e4
Jeremy Falling <jjfalling@gmail.com>
2013-03-03 11:04:52 -0800

    Update cisco_check_ip.pl

--- cisco_check_ip.pl
+++ cisco_check_ip.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/perl
-#this is used to see if an ip may or may not be free
+
+#****************************************************************************
+#*   cisco_check_ip                                                         *
+#*   Use with a cisco device to extensively check if an ip is in use.       *
+#*                                                                          *
+#*   Copyright (C) 2013 by Jeremy Falling except where noted.               *
+#*                                                                          *
+#*   This program is free software: you can redistribute it and/or modify   *
+#*   it under the terms of the GNU General Public License as published by   *
+#*   the Free Software Foundation, either version 3 of the License, or      *
+#*   (at your option) any later version.                                    *
+#*                                                                          *
+#*   This program is distributed in the hope that it will be useful,        *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+#*   GNU General Public License for more details.                           *
+#*                                                                          *
+#*   You should have received a copy of the GNU General Public License      *
+#*   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+#****************************************************************************
+
 
 use strict;
 use warnings;
